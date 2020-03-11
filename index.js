33186a39c93e98ed03d726327db1a40d59fe5974
ghpark <ghpark@hibrain.net>
2020-03-11 16:19:40 +0900

    action, function 정의

--- index.js
+++ index.js
@@ -2,8 +2,48 @@
 console.log('hello parcel');
 */
 
+// DOM 레퍼런스 생성
 const divToggle = document.querySelector('.toggle');
 const counter = document.querySelector('h1');
 const btnIncrease = document.querySelector('#increase');
 const btnDecrease = document.querySelector('#decrease');
 
+// 액션 정의
+const TOGGLE_SWITCH = 'TOGGLE_SWITCH';
+const INCREASE = 'INCREASE';
+const DECREASE = 'DECREASE';
+
+// 액션 생성함수 작성
+const toggleSwitch = () => ({type: TOGGLE_SWITCH});
+const increase = difference => ({type: INCREASE, difference});
+const decrease = () => ({type: DECREASE});
+
+// 초기값 설정
+const initialState = {
+    toggle: false,
+    counter: 0
+};
+
+// 리듀스 함수 정의
+
+function reducer(state = initialState, action) {
+    switch (action.type){
+        case TOGGLE_SWITCH:
+            return{
+                ...state,
+                toggle: !state.toggle
+            }
+        case INCREASE:
+            return{
+                ...state,
+                counter: state.counter + action.difference
+            }
+        case DECREASE:
+            return{
+                ...state,
+                counter: state.counter - 1
+            }
+        default:
+            return state;
+    }
+}
